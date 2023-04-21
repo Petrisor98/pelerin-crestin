00653ca7dd2f6f042ab36b6c122c68ab33c9a350
Petrisor98 <63288874+Petrisor98@users.noreply.github.com>
2023-04-21 19:50:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector(".menu").addEventListener("click", () => {
+    document.querySelectorAll(".target").forEach((item) => {
+        item.classList.toggle("change");
+    });
+});
+
+document.querySelectorAll(".wrapper").forEach((item) => {
+    item.addEventListener("click", () => {
+        document.querySelectorAll(".target").forEach((item) => {
+            item.classList.remove("change");
+        });
+    });
+});
+
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName('form')) {
+        form.reset();
+    }
+}
